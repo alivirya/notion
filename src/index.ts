4449ba7418a9ce0c52f59199113fa725b987c79d
Audrey Livirya <audrey.livirya@xero.com>
2021-09-27 21:21:01 +1300

    Added new page to db

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,54 @@
 import { Client } from "@notionhq/client";
-import { config } from "dotenv";
+import dotenv from "dotenv";
 
-config();
+dotenv.config();
+
+const calendarDb = process.env.CALENDAR_DB!;
 const notion = new Client({
   auth: process.env.NOTION_TOKEN,
 });
 
-const listUsers = await notion.users.list({});
-console.log(listUsers);
+// const options = yargs
+//   .usage("Usage: -c <name>")
+//   .option("n", {
+//     alias: "name",
+//     describe: "Your name",
+//     type: "string",
+//     demandOption: true,
+//   })
+//   .option("s", {
+//     alias: "search",
+//     describe: "Search term",
+//     type: "string",
+//   }).argv;
+
+const createNewCalendarEntry = async () => {
+  const response = await notion.pages.create({
+    parent: {
+      database_id: calendarDb,
+    },
+    properties: {
+      Name: {
+        title: [
+          {
+            text: {
+              content: "Test Entry",
+            },
+          },
+        ],
+      },
+      Tags: {
+        multi_select: [
+          {
+            name: "Reminder",
+          },
+        ],
+      },
+      Date: {
+        date: { start: new Date().toISOString() },
+      },
+    },
+  });
+};
+
+await createNewCalendarEntry();
